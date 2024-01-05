d7e0a078f015a5a1fd70c34ba5ff0951ba926e4b
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-05 19:03:19 -0300

    Operador Lógico

---
+++ 1_Projetos/8_boolean_op_logico/scripts.js
@@ -0,0 +1,10 @@
+console.log( 5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 === 1);
+
+console.log(3 === 3 && "nickolas" === "nickolas")
+console.log("Felipe" === "joão" || false);
+
+console.log(!(true && true));
+
+console.log(true && true);
+console.log( false || false);
