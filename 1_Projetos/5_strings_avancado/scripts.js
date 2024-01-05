f157d9dd06d9141618eee8066f64a2c9f16ed9fe
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-05 17:44:58 -0300

    String avançado

---
+++ 1_Projetos/5_strings_avancado/scripts.js
@@ -0,0 +1,5 @@
+console.log(" Primeira linha \n Segunda linha");
+console.log('O meu nome é "Nickolas"');
+console.log("O nome do cachorro é 'dog'");
+console.log(`a soma de 2 + 2 é: ${2 + 2}`);
+console.log("o " + "JavaScript " + "e " + "HTML");
