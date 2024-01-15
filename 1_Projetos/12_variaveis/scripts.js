705dbfc72b92df0aafbc7583587c960fec846a2d
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-15 19:10:20 -0300

    Variáveis

---
+++ 1_Projetos/12_variaveis/scripts.js
@@ -0,0 +1,31 @@
+let nome = 'Nick';
+
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5
+
+console.log (laranjas * laranjas);
+
+nome = "Andre";
+
+console.log(nome);
+
+laranjas = 342342
+
+console.log(typeof laranjas);
+console.log(laranjas);
+
+laranjas = "laranjas";
+
+console.log(typeof laranjas);
+console.log(laranjas);
+
+
+const sobrenome = "Andre";
+
+console.log(sobrenome);
+
+const numero1 = 10;
+
+console.log(numero1);
