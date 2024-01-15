3e0054ca54fd5007cf8ef7308ca67f70dff9e708
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-15 20:57:26 -0300

    function

---
+++ 1_Projetos/13_prompt/scripts.js
@@ -0,0 +1,17 @@
+ let idade = prompt("sua idade");
+
+ console.log(idade);
+
+ let nome = prompt("Qual é o seu nome?");
+
+ console.log(`O seu nome é ${nome}`)
+
+// alert
+
+alert("Esta é a mensagem!");
+
+// math.x()
+
+let maior = Math.max(1,4,6,2,6);
+
+console.log(maior);
