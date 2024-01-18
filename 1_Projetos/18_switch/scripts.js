e14adbb273ebf1e565f48a2dfc0b915513e41e70
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-17 22:03:36 -0300

    switch

---
+++ 1_Projetos/18_switch/scripts.js
@@ -0,0 +1,13 @@
+let nome = "nick";
+
+switch(nome){
+    case "nick":
+        console.log("nick");
+        break;
+    case "andre":
+        console.log("andre");
+        break;
+    default:
+        console.log("nickkk e andre");
+        break;
+}
