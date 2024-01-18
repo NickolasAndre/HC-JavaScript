5b278f779888d8a9823c0db2e920c301f6c64ff1
Nickolas Andre <nickolas.sam3@gmail.com>
2024-01-17 21:14:17 -0300

    for

---
+++ 1_Projetos/15 - for/scripts.js
@@ -0,0 +1,23 @@
+let a = 0;
+
+for(a = 0; a <= 10; a ++){
+    console.log(a);
+}
+
+let b = 10;
+
+for(b = 10; b >= 0; b --){
+    console.log(b)
+}
+
+let c = 2;
+
+for(c = 2; c <= 100; c = c * 2){
+    console.log(c)
+}
+
+let d = 0;
+
+for(d = 0; d < 100; d = d + 3){
+    console.log(3);
+}
