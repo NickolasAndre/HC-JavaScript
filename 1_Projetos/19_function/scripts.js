6292fb8ca42b13b790c8020add0e7b5a7f858fcd
Nickolas Andre <nickolas.sam3@gmail.com>
2024-02-01 21:28:58 -0300

    Função
    Definir função

---
+++ 1_Projetos/19_function/scripts.js
@@ -0,0 +1,35 @@
+function imprimirNoConsole(){
+    console.log("Olá mundo!");
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num){
+    console.log("O número é: " + num);
+}
+
+imprimirUmNumero(3);
+
+function imprimirUmNumero1(num1){
+    console.log('Outro número: ' + num1);
+}
+
+imprimirUmNumero1(4);
+
+const numeroAleatorio = function(){
+    console.log(Math.random());
+}
+
+numeroAleatorio();
+
+const textoAle = function(texto){
+    console.log(texto);
+}
+
+textoAle("texto");
+
+const alea = function(numm){
+    console.log(numm);
+}
+
+alea("1423");
