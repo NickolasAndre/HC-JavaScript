a193e94887f1191247c691be978c33ac80eb1897
Nickolas Andre <nickolas.sam3@gmail.com>
2024-02-05 19:10:52 -0300

    Mais sobre funções

--- 1_Projetos/19_function/scripts.js
+++ 1_Projetos/19_function/scripts.js
@@ -33,3 +33,44 @@ const alea = function(numm){
 }
 
 alea("1423");
+
+// Mais sobre funções
+
+const soma = function(a, b){
+    return a + b;
+}
+
+console.log(soma(5,5));
+
+const somaa = soma(3,6);
+
+console.log("O valor de soma: " + somaa)
+
+
+const saudacao = function(nome){
+    if(nome == "Nico")
+    return "Olá Nico"
+}
+
+console.log(saudacao("Nico"));
+
+const sau = function(tt){
+    if(tt == "Nicoo");
+    return "Oi nicoo";
+}
+
+console.log(sau("Nicoo"));
+
+function podeDirigir(idade, cnh){
+    if(idade >= 18 && cnh == true){
+        console.log("pode dirigir!");
+    }else{
+        console.log("Não pode dirigir!");
+    }
+}
+
+podeDirigir(22, true);
+podeDirigir(25, true);
+podeDirigir(44, 0);
+podeDirigir(22, 1);
+podeDirigir(15, true);
