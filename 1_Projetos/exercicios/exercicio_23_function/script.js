0adaa3cdd36ca6fccf158375a2c0a2db59c12774
Nickolas Andre <nickolas.sam3@gmail.com>
2024-05-27 20:53:48 -0300

    commit

--- 1_Projetos/exercicios/exercicio_23_function/script.js
+++ 1_Projetos/exercicios/exercicio_23_function/script.js
@@ -8,3 +8,48 @@ function calculateAverage(numeros) {
 }
 
 console.log(calculateAverage([1, 2, 3, 4, 5]));
+
+
+function countVowels(str) {
+    let count = 0;
+    const vowels = 'aeiouAEIOU';
+
+    for(let i = 0; i < str.length; i++) {
+        if(vowels.includes(str[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countVowels("countVowels"))
+
+
+function contarVogais(vogais){
+
+    let contar = 0;
+    const vogaisT = "aeiouAEIOU"
+
+    for(i = 0; i < vogais.length; i++){
+        if(vogaisT.includes(vogais[i])){
+            contar++
+        }
+    }
+    return contar
+}
+
+console.log(contarVogais("Hello World"));
+
+
+
+function sumEvenNumbers(numbers) {
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++) {
+        if(numbers[i] % 2 === 0) {
+            sum += numbers[i];
+        }
+    }
+    return sum;
+}
+
