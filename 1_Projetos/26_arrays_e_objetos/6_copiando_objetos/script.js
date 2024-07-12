5d3bdd7b3336a7b239f7e71d373a7a5add773bc9
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-11 21:26:31 -0300

    assign

---
+++ 1_Projetos/26_arrays_e_objetos/6_copiando_objetos/script.js
@@ -0,0 +1,40 @@
+let objetoA = {
+    prop1: "teste",
+    prop2: "testando"
+};
+
+let objetoB = {
+    prop3: "propriedade"
+}
+
+console.log(objetoA);
+console.log(objetoB);
+
+Object.assign(objetoA, objetoB);
+
+console.log(objetoA);
+
+
+
+
+
+
+
+let carro = {
+    porta: 2,
+    marca: "VW",
+    cor: "Verde"
+}
+
+let carro2 = {
+    motor: 1.2
+}
+
+Object.assign(carro, carro2);
+
+console.log(carro);
+
+
+
+
+
