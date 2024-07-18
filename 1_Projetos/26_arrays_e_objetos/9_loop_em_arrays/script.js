d54b2b207f37280285c50d66387a5c0dded66653
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-17 21:10:27 -0300

    loop em arrays

---
+++ 1_Projetos/26_arrays_e_objetos/9_loop_em_arrays/script.js
@@ -0,0 +1,11 @@
+let numeros = [1, 5, 10, 15, 20, 25];
+
+for(i = 0; i < numeros.length; i++){
+    console.log(numeros[i])
+}
+
+let carros = ["carro", "moto", "bicicleta"];
+
+for( j = 0 ; j < carros.length ; j++){
+    console.log(carros[j])
+}
