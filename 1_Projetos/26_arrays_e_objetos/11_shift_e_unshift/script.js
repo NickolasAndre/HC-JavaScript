c0926b594394bf3591d8605403d73621bb7c8d01
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-18 21:26:08 -0300

    shift e unshift

---
+++ 1_Projetos/26_arrays_e_objetos/11_shift_e_unshift/script.js
@@ -0,0 +1,11 @@
+let pessoas = ["nickolas","andre","josé"];
+
+let pessoaRemovda = pessoas.shift();
+
+console.log(pessoaRemovda);
+
+console.log(pessoas);
+
+pessoas.unshift("maria");
+
+console.log(pessoas)
