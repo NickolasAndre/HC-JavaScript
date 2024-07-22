5dc0542b88ac7c3b0b18dd7700981163948fe1ce
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-21 21:31:41 -0300

    slice

---
+++ 1_Projetos/26_arrays_e_objetos/13_slice/script.js
@@ -0,0 +1,13 @@
+let numeros = [0, 1, 2, 3, 4, 5];
+
+console.log(numeros.slice(2,3));
+
+console.log(numeros.slice(3));
+
+console.log(numeros.slice(3,4));
+
+console.log(numeros.slice(2,4));
+
+console.log(numeros.slice(-2));
+
+console.log(numeros.slice(2, -2));
