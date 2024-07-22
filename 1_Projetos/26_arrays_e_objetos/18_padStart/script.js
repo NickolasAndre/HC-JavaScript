a000ac9d345209daa846541dc07f121d9b9356df
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-22 20:44:11 -0300

    padStart

---
+++ 1_Projetos/26_arrays_e_objetos/18_padStart/script.js
@@ -0,0 +1,11 @@
+let milAoContrario = "1";
+
+console.log(milAoContrario.padStart(4, "0"));
+
+let numeros = "2";
+
+console.log(numeros.padStart(6, "0"));
+
+let letras = "A";
+
+console.log(letras.padStart(3, "B"));
