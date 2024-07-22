1d97ab950515b32350199ef592d772be73f72c14
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-22 20:54:28 -0300

    split

---
+++ 1_Projetos/26_arrays_e_objetos/19_split/script.js
@@ -0,0 +1,11 @@
+let frase = "O rato roeu a roupa do rei de Roma";
+
+console.log(frase.split(" "));
+
+let fraseNumero = "Um Dois Três quatro cinco";
+
+console.log(fraseNumero.split(" "));
+
+let numero = "1 2 3 4 5 6 7 8";
+
+console.log(numero.split(" "));
