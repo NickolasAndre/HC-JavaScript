b5259696257e324cc73ad8dbd09c83f13e61fcd0
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-21 21:18:12 -0300

    indexOf e lastIndexOf

---
+++ 1_Projetos/26_arrays_e_objetos/12_indexOf_e_lastIndexof/script.js
@@ -0,0 +1,11 @@
+let numeros = [1, 2, 3, 4, 5, 6];
+
+console.log(numeros.indexOf(3));
+
+console.log(numeros.lastIndexOf(1))
+
+let nome = ["João","Maria","Jóse","Nico", "João"];
+
+console.log(nome.indexOf("Nico"));
+
+console.log(nome.lastIndexOf("João"))
