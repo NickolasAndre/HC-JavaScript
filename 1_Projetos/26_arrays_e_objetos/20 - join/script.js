c7c892a3bc891b8cffe5438dcb36d7d54fee1f85
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-22 21:37:07 -0300

    join

---
+++ 1_Projetos/26_arrays_e_objetos/20 - join/script.js
@@ -0,0 +1,11 @@
+let frase = "Testando o método splint";
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let novaFrase = palavras.join("@");
+
+console.log(novaFrase);
+
+console.log(palavras.join(" "));
