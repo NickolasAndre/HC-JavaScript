3b30a6a83bed381d05147a1d992bf8e895a644dd
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-24 21:13:00 -0300

    Destructuring em objetos

---
+++ 1_Projetos/26_arrays_e_objetos/23_destructuring_objetos/script.js
@@ -0,0 +1,36 @@
+let obj = {
+    rodas: 4,
+    portas: 4,
+    tetoSolar: true,
+    motor: "1.0"
+};
+
+const {rodas: vRodas, portas: vPortas, tetoSolar: vTetoSolar, motor: vMotor} = obj;
+
+console.log(vRodas);
+console.log(vPortas);
+console.log(vTetoSolar);
+console.log(vMotor);
+
+
+
+
+
+let animais = {
+    cachorro: "dog",
+    gato: "cat",
+    cavalo: "horse"
+};
+
+const {cachorro: adog, gato: acat, cavalo: ahorse} = animais;
+
+console.log(adog);
+console.log(acat);
+console.log(ahorse);
+
+
+
+
+
+
+
