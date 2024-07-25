499ccbf20c3bd74f63de67007afc1b176b2f6762
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-24 21:33:54 -0300

    Destructuring em array

---
+++ 1_Projetos/26_arrays_e_objetos/24_destructuring_em_array/script.js
@@ -0,0 +1,28 @@
+let nomes = ["Nickolas","Andre","Pedro", "Gabriel"];
+
+let [nomeA, nomeB, nomeC, nomeD] = nomes;
+
+console.log(nomeA);
+console.log(nomeB);
+console.log(nomeC);
+console.log(nomeD);
+
+
+
+
+
+
+let animais = ["Gato","Cachorro","Cavalo","Pato"];
+
+let [GatoA, CachorroA, CavaloA, PatoA] = animais;
+
+console.log(GatoA)
+console.log(CachorroA)
+console.log(CavaloA)
+console.log(PatoA)
+
+
+
+
+
+
