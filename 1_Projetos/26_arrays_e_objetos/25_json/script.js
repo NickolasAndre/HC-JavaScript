f7d9de78be46566040e04efa69ada5cd4ce27581
Nickolas Andre <nickolas.sam3@gmail.com>
2024-07-24 21:52:00 -0300

    JSON

---
+++ 1_Projetos/26_arrays_e_objetos/25_json/script.js
@@ -0,0 +1,40 @@
+let pessoa = {
+    "nome": "Nickolas",
+    "sobrenome": "Andre",
+    "idade": 25,
+    "animias": ["gato","cachorro","cavalo"]
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.sobrenome);
+console.log(pessoa.idade);
+console.log(pessoa.animias);
+
+
+
+
+
+
+
+
+
+
+
+
+let num = {
+    "um": 1,
+    "dois": 2,
+    "1.1": true,
+    "1 a 4": [1, 2, 3, 4]
+};
+
+console.log(num.um);
+console.log(num.dois);
+console.log(num["1.1"]);
+console.log(num["1 a 4"]);
+
+
+
+
+
+
