403aa1f5c24bd70d9a198319c136e66943373c55
Nickolas Andre <nickolas.sam3@gmail.com>
2024-09-13 21:51:37 -0300

    class construtor

---
+++ 1_Projetos/27_conceitos_de_orientacao_a_objetos/9_construtor_class/script.js
@@ -0,0 +1,12 @@
+class cachorro{
+    constructor(raca, patas){
+        this.raca = raca
+        this.patas = patas
+    }
+}
+
+let labrador = new cachorro("labrador", 4);
+
+console.log(labrador)
+console.log(labrador.raca);
+console.log(labrador.patas);
