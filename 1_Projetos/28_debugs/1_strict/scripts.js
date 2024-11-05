5f2471a27c29c7768b0dae2359af6125a72444ed
NickolasAndre <nickolas.sam3@gmail.com>
2024-11-04 21:32:17 -0300

    Strict

---
+++ 1_Projetos/28_debugs/1_strict/scripts.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let opa = "teste"
+
+//delete Object.prototype
+
+function teste(){
+    "use strict"
+
+  let  testando = "teste"
+
+}
+
+teste()
+
+//false.prop = "";
+//teste.prop = "";
